b7a6f076be4575e135838552a59b08bc4b822ab6
Abel <abel@perkfix.com>
2020-07-14 19:15:49 -0400

    Updates

---
+++ precache-manifest.1e37b8a16a1128ae94420987c5cdf50d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376ab846ac299673069f4311c47ea7ca",
+    "url": "/reactAppLanding/index.html"
+  },
+  {
+    "revision": "36a141998ae6686809dd",
+    "url": "/reactAppLanding/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "0195d00c33e854e6cd4b",
+    "url": "/reactAppLanding/static/css/main.0b81113b.chunk.css"
+  },
+  {
+    "revision": "36a141998ae6686809dd",
+    "url": "/reactAppLanding/static/js/2.c7acb2f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactAppLanding/static/js/2.c7acb2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0195d00c33e854e6cd4b",
+    "url": "/reactAppLanding/static/js/main.8b138403.chunk.js"
+  },
+  {
+    "revision": "522075ec362057bdf0a2",
+    "url": "/reactAppLanding/static/js/runtime-main.38661289.js"
+  },
+  {
+    "revision": "ea294b37d4a321d248e2ec65053d4317",
+    "url": "/reactAppLanding/static/media/amazon.ea294b37.png"
+  },
+  {
+    "revision": "23c9bc6069e34dad23901d5281e90749",
+    "url": "/reactAppLanding/static/media/copyright.23c9bc60.png"
+  },
+  {
+    "revision": "7d2650085109954ec1d0504f96e6aff3",
+    "url": "/reactAppLanding/static/media/gift.7d265008.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactAppLanding/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96a682e18e74836bacf73a2be1a1757b",
+    "url": "/reactAppLanding/static/media/netflix.96a682e1.png"
+  },
+  {
+    "revision": "4f77c70233f9ce33d4ebed4d26c45446",
+    "url": "/reactAppLanding/static/media/phone.4f77c702.png"
+  },
+  {
+    "revision": "d84c7c48f14d69bd89a2b55c3bc36c2f",
+    "url": "/reactAppLanding/static/media/robin.d84c7c48.png"
+  },
+  {
+    "revision": "a1076bf44d8dd35591120da6d9b71f7c",
+    "url": "/reactAppLanding/static/media/spotify.a1076bf4.png"
+  },
+  {
+    "revision": "2491fa0d02b88d255ed008b4bcd7a23a",
+    "url": "/reactAppLanding/static/media/starbucks.2491fa0d.png"
+  }
+]);
